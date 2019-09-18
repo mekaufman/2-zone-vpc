483cd1acda204a9af1f2cea81923e13c25f76722
mekaufman <mtompki@us.ibm.com>
2019-09-18 08:47:36 -0400

    edits

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 
 variable "ibmcloud_api_key" {
-  description = "Your IBM Cloud API key."
+  description = "Your IBM Cloud API key. If you need to create one, go to https://cloud.ibm.com/iam/apikeys."
 }
 
 variable "region" {
-  description = "Your region to deploy."
+  description = "Region to deploy VPC"
   default = "us-east"
 }
 
